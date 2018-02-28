dbe2e8d7a69d9ae999d50ef021ebd4f85f9924f7
wollexvevo <36920032+wollexvevo@users.noreply.github.com>
2018-02-28 14:29:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,15 @@
-const { stripIndents, oneLine } = require('common-tags');
-const Discord = require("discord.js");
-const bot = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-let prefix = "!";
-let owner = "355435506554765312"; //
-
-bot.on("ready", () => {
-    bot.user.setGame( prefix +"yardım | Wollex Team <3") 
-    console.log("Wollex Bot v1.0.0 Sürümü ile Botun Paneli Aktif Hala Geldi")   
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.login("NDE4MzY0MDc0NTE0OTcyNjky.DXgfcA.dEU21hZ1YsxN8YDN1S6LmQSpJ40")
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
-bot.on("message", message => {
-
-    if (message.content.toLowerCase() === "sa") {
-        message.reply("**Aleyküm Selam Birader ;]**")
-    }
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
