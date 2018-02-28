44104b089ec3f071a132e35439955475ddb41f98
wollexvevo <36920032+wollexvevo@users.noreply.github.com>
2018-02-28 18:58:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ('+')
 
+  client.user.setGame(`+komutlar | Beta Aşaması`);
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
