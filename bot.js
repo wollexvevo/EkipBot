e328dffa88d64b10382a911017947215f711246e
wollexvevo <36920032+wollexvevo@users.noreply.github.com>
2018-02-28 15:44:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,5 +18,32 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'sunucubilgi') {
+    if  (msg.channel.type === 'dm') {
+      const ozelmesajuyarii = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .addField(' Uyarı ', 'Bu komutu özel mesajlarda kullanamazsın.')
+    msg.author.sendEmbed(ozelmesajuyarii); }
+    if (msg.channel.type !== 'dm') {
+      const sunucubilgi = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(msg.guild.name, msg.guild.iconURL)
+    .addField('Ad:', msg.guild.name)
+    .addField('ID', msg.guild.id)
+    .addField('Ana kanal:', msg.guild.defaultChannel)
+    .addField('Bölge', msg.guild.region)
+    .addField('Üye sayısı:', msg.guild.memberCount)
+    .addField('Sahibi:', msg.guild.owner)
+    .addField('Kanal sayısı:', msg.guild.channels.size)
+    .addField('Oluşturulma tarihi:', msg.guild.createdAt)
+    return  msg.channel.sendEmbed(sunucubilgi);
+    }
+  }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
