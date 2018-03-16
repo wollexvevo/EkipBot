1f990295d9fe6ece0c222a766d05d3f5697f3c4e
wollexvevo <36920032+wollexvevo@users.noreply.github.com>
2018-03-16 18:21:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,6 +80,26 @@ client.on('message', message => {
     return message.author.sendEmbed(pingozel);
   }
 });
+client.on('message', message => {
+  if (message.content.toLowerCase() === prefix + 'kullanıcıbilgim')
+    if (message.channel.type !== "group") {
+        var Durum = message.author.presence.status;
+        var Durm = (Durum == "online" ? (0x00AE86) : (Durum == "offline" ? (0x808080) : (Durum == "idle" ? (0xFFFF00) : (Durum == "dnd" ? (0xFF0000) : (0x00AE86)))))
+        var durm = (Durum == "online" ? ("Çevrimiçi") : (Durum == "offline" ? ("Çevrimdışı") : (Durum == "idle" ? ("Boşta") : (Durum == "dnd" ? ("Rahatsız Etmeyin") : ("Bilinmiyor/bulunamadı.")))))
+      const kullanicibilgimk = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setColor(Durm)
+      .setTimestamp()
+      .addField('Ad:', message.author.username + '#' + message.author.discriminator)
+      .addField('ID:', message.author.id)
+      .addField('Kayıt tarihi:', message.author.createdAt)
+      .addField('Durum:', durm)
+      .addField('Şu an oynadığı oyun:', message.author.presence.game ? message.author.presence.game.name : 'Şu an oyun oynamıyor')
+      .addField('BOT mu?', message.author.bot ? '\n Evet' : 'Hayır')
+      console.log("!kullanıcıbilgim komutu " + message.author.username + " tarafından kullanıldı.")
+      return message.channel.sendEmbed(kullanicibilgimk);
+  }
+});
 
 // AĞLAYIN :D
 client.login(process.env.BOT_TOKEN);
