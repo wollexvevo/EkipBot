0d9da8dd01d0345e8d2e73a97ea1aa6e6019ffe9
wollexvevo <36920032+wollexvevo@users.noreply.github.com>
2018-03-16 17:01:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,8 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'sa') {
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
     	message.reply('**Aleyküm Selamm, sunucumuza hoşgeldin** :ballot_box_with_check: ');
   	}
 });
@@ -43,13 +43,39 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === '+komutlar') {
     	message.channel.send('**Botun Ana Komutları**"');
-      message.channel.send('"w!yardım = BOT Komutlarını Atar. \nw!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nw!ping = BOT Gecikme Süresini Söyler. \nw!davet = BOT Davet Linkini Atar. \nw!istatistik = BOT İstatistiklerini Atar.');
+      message.channel.send('w!yardım = BOT Komutlarını Atar. \nw!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nw!ping = BOT Gecikme Süresini Söyler. \nw!davet = BOT Davet Linkini Atar. \nw!istatistik = BOT İstatistiklerini Atar.');
   	}
 });
 client.on('message', message => {
     if (message.content === '+davet') {
     	message.reply('**Davet Linkim Davet Et Hemen Gelirim**<3 | https://goo.gl/9vcQai ');
   	}
+});
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === '+sunucubilgi') {
+    if  (msg.channel.type === 'dm') {
+      const ozelmesajuyarii = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .addField(':warning: Uyarı :warning:', 'Bu komutu özel mesajlarda kullanamazsın.')
+    msg.author.sendEmbed(ozelmesajuyarii); }
+    if (msg.channel.type !== 'dm') {
+      const sunucubilgi = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(msg.guild.name, msg.guild.iconURL)
+    .addField('Ad:', msg.guild.name)
+    .addField('ID', msg.guild.id)
+    .addField('Ana kanal:', msg.guild.defaultChannel)
+    .addField('Bölge', msg.guild.region)
+    .addField('Üye sayısı:', msg.guild.memberCount)
+    .addField('Sahibi:', msg.guild.owner)
+    .addField('Kanal sayısı:', msg.guild.channels.size)
+    .addField('Oluşturulma tarihi:', msg.guild.createdAt)
+    return  msg.channel.sendEmbed(sunucubilgi);
+    }
+  }
 });
  
 // AĞLAYIN :D
